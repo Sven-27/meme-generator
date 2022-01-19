c117b7b21603e23656b6a30b5bee67010c9e47fd
Sven-27 <sven.notermans@live.nl>
2022-01-19 22:12:01 +0100

    finished header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import '../styles/Header.scss' 
+import trol from 'images/troll-face.png'
 
 const Header = () => {
   return (
   <div className='header'>
-
+    <img src={ trol } alt="icon troll face" />
+    <h2>Meme Generator</h2>
+    <p>React Course - Project 3</p>
   </div>
   )
 }
