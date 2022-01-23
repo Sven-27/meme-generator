6257ba52e4ee756bbdc61451086b2c64f3b6b269
Sven-27 <sven.notermans@live.nl>
2022-01-23 12:20:37 +0100

    made images display on screen

--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -1,14 +1,16 @@
+import React from 'react'
 import '../styles/Meme.scss'
 import ModelObject from 'data/memesData'
 
 const Meme = () => {
+  const [ getRandomImage, setGetRandomImage ] = React.useState('')
   console.log(ModelObject.data.memes)
   
   const randomImage = () => {
     const memesArray = ModelObject.data.memes
     const randomImage = Math.floor(Math.random() * memesArray.length)
     const url = memesArray[randomImage].url
-    console.log(url)
+    setGetRandomImage(url)
   }
 
   return (
@@ -31,9 +33,17 @@ const Meme = () => {
           onClick={ randomImage }
         >
           Get a new meme image 
-          <i className="fas fa-hand-middle-finger" />
+          <i className='fas fa-hand-middle-finger' />
         </button>
       </div>
+        {
+          getRandomImage &&
+           <img 
+              className='meme--random-image'
+              src={ getRandomImage } 
+              alt='' 
+            />
+        }
     </main>
   )
 }
